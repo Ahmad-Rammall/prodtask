9b9462d4bbb528d0ee60faf1110d66c699c9a96a
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-08-20 00:45:10 +0300

    Add Header / Footer to Main Layout

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../../components/layout/header/header.component";
+import { FooterComponent } from "../../components/layout/footer/footer.component";
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
