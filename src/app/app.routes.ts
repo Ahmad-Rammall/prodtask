4d22c6402db3921a2ffc4873ebbf7c243edce456
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-08-19 23:42:22 +0300

    Create Login + Main Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './screens/login/login.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent }, // Example route for a page with header and footer
+      // Add more routes here for pages with header and footer
+    ],
+  },
+];
